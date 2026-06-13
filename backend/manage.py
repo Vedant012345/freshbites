3adf77a7102c0b9e4894ae49b5c523941d39b643
Pratiksha <pratikshamhaske86@gmail.com>
2026-06-13 14:54:31 +0530

    backend

--- backend/manage.py
+++ backend/manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    # Points cleanly to your split settings directory (bitestreak/settings/base.py)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bitestreak.settings.base")
     try:
         from django.core.management import execute_from_command_line
